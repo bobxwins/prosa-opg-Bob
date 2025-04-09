e59b1a90be92e6082a91499da2268684c77e5031
bobxwins <bob-wins@hotmail.com>
2025-04-09 23:00:12 +0200

    - Added Customer list to Program.cs
    - Added WhenUsingMockDataAccessServiceAreWeGettingFiveCustomers test

--- ProsaOpg.ConsoleUI/Program.cs
+++ ProsaOpg.ConsoleUI/Program.cs
@@ -4,4 +4,8 @@ System.Console.WriteLine("Hello, World! - debug");
 #else
     System.Console.WriteLine("Hello, World! - release");
 #endif
+var dataAccessService = DataAccessFactory.CreateDataContext("Data Source=customers.db;Mode=ReadOnly");
+var customers= dataAccessService.GetAllCustomers();
+foreach(var customer in customers)
+ System.Console.WriteLine(customer.Name);
 
--- ProsaOpg.Test/AllTests.cs
+++ ProsaOpg.Test/AllTests.cs
@@ -11,5 +11,11 @@ public class AllTests
         var kunde = new Customer();
         Assert.True(kunde.CreatedDate.Date == DateTime.Today.Date, "CreatedDate should be today's date.");
     }
-
+    [Fact]
+    public void WhenUsingMockDataAccessServiceAreWeGettingFiveCustomers ()
+    {
+        MockDataAccessService dataAccessService = new MockDataAccessService();
+        var customers= dataAccessService.GetAllCustomers();
+        Assert.True(customers.Count() == 5);
+    }
 }
